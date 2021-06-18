5979c63db612a0da60b8e229980e0373d672a248
pritam3196 <40634060+pritam3196@users.noreply.github.com>
2021-06-18 10:47:09 +0530

    Add files via upload

---
+++ face_detector/face_detector.py
@@ -0,0 +1,34 @@
+import cv2
+from random import randrange
+
+#load some pretrained data from harcascade
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#choose an image to detect the faces in
+#img = cv2.imread('1.jpg')
+
+#to capture video from webcam
+webcam=cv2.VideoCapture(0) #0 is the default webcam or a video file
+
+#must convert to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#detect the faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+#draw rectangles around the faces
+#the last 2 is the thickness, 10 is the thickest. The second tuple is the
+#size of the sqaure so we can add it to the coordnates of the upper left hand coordinate
+#cv2.rectangle(img, (48,92), (48+232,92+232), (0,255,0), 2)
+
+for (x,y,w,h) in face_coordinates:
+	cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+#print(face_coordinates)
+
+#display the images with the faces
+cv2.imshow('Basic face detector', img)
+cv2.waitKey()
+
+
+
+print('Code completed')
